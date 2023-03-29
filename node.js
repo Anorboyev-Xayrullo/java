eb1f4d33fa99f09684e19b1b5d8b8a521c68d025
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2023-03-29 11:12:39 +0500

    fix

---
+++ node.js
@@ -0,0 +1,16 @@
+let inp = document.querySelector("input")
+let btn = document.querySelector("button")
+
+
+
+
+
+btn.onclick = () =>{
+    if(inp.value == "1234"){
+        console.log("to'g'ri")
+    }else{
+        console.log("notog'ri")
+    }
+}
+
+
